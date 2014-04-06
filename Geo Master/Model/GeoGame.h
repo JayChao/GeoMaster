ebd367ed009987862b3882fd9c838683e13b279e
Joe Volker <joseph-volker@uiowa.edu>
2014-04-06 14:05:40 -0500

    “initial commit”

---
+++ Geo Master/Model/GeoGame.h
@@ -0,0 +1,13 @@
+//
+//  GeoGame.h
+//  Geo Master
+//
+//  Created by Volker, Joseph on 4/6/14.
+//  Copyright (c) 2014 ios.uiowa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GeoGame : NSObject
+
+@end
---
+++ Geo Master/Model/GeoGame.m
@@ -0,0 +1,15 @@
+//
+//  GeoGame.m
+//  Geo Master
+//
+//  Created by Volker, Joseph on 4/6/14.
+//  Copyright (c) 2014 ios.uiowa. All rights reserved.
+//
+
+#import "GeoGame.h"
+
+@implementation GeoGame
+
+
+
+@end
