da66fbf45fe65d50ceefc39da80a1c21d8ffb08e
jaychao <jay118chao@gmail.com>
2014-04-06 17:31:42 -0500

    map view

--- Geo Master/FitBoundsViewController.m
+++ Geo Master/FitBoundsViewController.m
@@ -3,12 +3,13 @@
 #endif
 
 #import "FitBoundsViewController.h"
-///#import "GeoGame"
+#import "GeoGame.h"
 
 #import <GoogleMaps/GoogleMaps.h>
 
 @interface FitBoundsViewController () <GMSMapViewDelegate>
 @property (nonatomic) BOOL thereIsAPin;
+@property (nonatomic) GeoGame *game;
 @end
 
 @implementation FitBoundsViewController {
@@ -19,15 +20,16 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:37.81969
+  GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:6.8
                                                           longitude:0.966085
                                                                zoom:4];
   mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
   mapView_.delegate = self;
   self.view = mapView_;
     [mapView_ clear];
-
-
+    self.game = [[GeoGame alloc]init];
+	NSLog(@"%@", [[self.game coordinatesToGuess] objectAtIndex:0]);
+    NSLog(@"%@", [[self.game coordinatesToGuess] objectAtIndex:1]);
 
   // Create a button that, when pressed, updates the camera to fit the bounds
   // of the specified markers.
@@ -62,11 +64,14 @@
   GMSMarker *marker = [[GMSMarker alloc] init];
   marker.title = [NSString stringWithFormat:@"Marker at: %.2f,%.2f",
                   coordinate.latitude, coordinate.longitude];
+    NSLog(@"titile %@",marker.title);
     
  //GeoGame.coordinateGiven = [
- //GeoGame.coordinatesToGuess = [NSArray arrayWithObjects:[[marker.title][0], [marker.title][1],nil]];
- //[self GeoGame];
-
+    NSArray *gameArray = [NSArray arrayWithObjects:[NSNumber numberWithDouble:coordinate.latitude], [NSNumber numberWithDouble:coordinate.longitude],nil];
+    
+    [self.game calculateScore:gameArray];
+    NSLog(@"print %@", [self.game score]);
+    
     marker.map = nil;
   marker.position = coordinate;
   marker.appearAnimation = kGMSMarkerAnimationPop;
