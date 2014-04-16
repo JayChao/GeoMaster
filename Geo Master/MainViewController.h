3f9ce5002c00e0aa5f20c1700f67257d8caf3251
Joe Volker <joseph-volker@uiowa.edu>
2014-04-15 19:44:09 -0500

    integrated Facebook login

--- Geo Master/MainViewController.h
+++ Geo Master/MainViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
 
-@interface MainViewController : UIViewController
+@interface MainViewController : UIViewController <FBLoginViewDelegate>
 
 @end
