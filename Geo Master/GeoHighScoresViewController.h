9797e57fbb8f1754ae18d662989a529e99763d83
Joe Volker <joseph-volker@uiowa.edu>
2014-04-30 19:28:15 -0500

    began working on high scores

---
+++ Geo Master/GeoHighScoresViewController.h
@@ -0,0 +1,13 @@
+//
+//  GeoHighScoresViewController.h
+//  Geo Master
+//
+//  Created by Volker, Joseph on 4/30/14.
+//  Copyright (c) 2014 ios.uiowa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GeoHighScoresViewController : UITableViewController
+
+@end
