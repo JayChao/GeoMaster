92c78fd3126a22fb1383119e0232de220005be60
Joe Volker <joseph-volker@uiowa.edu>
2014-05-01 20:20:07 -0500

    undid parse junk

--- Geo Master/GeoAppDelegate.m
+++ Geo Master/GeoAppDelegate.m
@@ -8,7 +8,6 @@
 
 #import "GeoAppDelegate.h"
 #import <GoogleMaps/GoogleMaps.h>
-#import <Parse/Parse.h>
 #import <FacebookSDK/FacebookSDK.h>
 #import "GeoRecord.h"
 #import "GeoRecordCollection.h"
@@ -27,9 +26,6 @@
     // Override point for customization after application launch.
     [FBLoginView class];
     [GMSServices provideAPIKey:@"AIzaSyCtGILRnz4sF78fb67SnfIRIvwi4U1e_XI"];
-    
-    [Parse setApplicationId:@"fYJvGJFZtNALTKWBvo4dxLZs4PUAFvZGEJ9PUfsZ"
-                  clientKey:@"y73Kosd3Uk1YZ1k4V5blz2h3HXDZBztseeQ0HcJ2"];
     
     [application registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge|
      UIRemoteNotificationTypeAlert|
@@ -91,15 +87,10 @@
 - (void)application:(UIApplication *)application
 didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
-    // Store the deviceToken in the current installation and save it to Parse.
-    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
-    [currentInstallation setDeviceTokenFromData:deviceToken];
-    [currentInstallation saveInBackground];
 }
 
 - (void)application:(UIApplication *)application
 didReceiveRemoteNotification:(NSDictionary *)userInfo {
-    [PFPush handlePush:userInfo];
 }
 
 @end
